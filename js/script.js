abff2e71778cc0957d5465a043b1ec78eacdb0b4
Mario <lombardomario01@gmail.com>
2021-11-04 15:14:57 +0100

    Added card Teams

--- js/script.js
+++ js/script.js
@@ -7,8 +7,70 @@ Ogni membro dovrà avere le informazioni necessarie per stampare la relativa car
 
 /*
 
-1.
-
+1. Creo un array di oggetti in cui inserirò i vari membri con le loro informazioni => const membersTeam = [{}];
+    1.2 Aggiungerò le varie informazioni (nome - ruolo - foto) negli objects => {'name': 'tizio', 'role': 'x', 'photo': 'img/.x'};
+2. Stampo le card in HTML => .append    
 */
 
-console.log("Eccomii, sono OUR TEAM!");
+let contCardImgTeam = document.querySelector('.card-image');
+let contCardTextTeam = document.querySelector('.card-text');
+let contMembersTeam = document.querySelector('.team-card');
+let contTeams = document.querySelector('.team-container');
+let cardMember = "";
+let cardTextMember = "";
+
+const membersTeam = [
+    {
+       'name': 'Wayne Barnett',
+       'role': 'Founder & CEO',
+       'photo': 'img/wayne-barnett-founder-ceo.jpg' 
+    },
+    {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'photo': 'img/angela-caroll-chief-editor.jpg' 
+     },
+     {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'photo': 'img/walter-gordon-office-manager.jpg' 
+     },
+     {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'photo': 'img/angela-lopez-social-media-manager.jpg' 
+     },
+     {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'photo': 'img/scott-estrada-developer.jpg' 
+     },
+     {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'photo': 'img/barbara-ramos-graphic-designer.jpg' 
+     }
+];
+
+console.log(membersTeam);
+for (let key in membersTeam) {
+    cardMember += `<img src="${membersTeam[key].photo}" alt="${membersTeam[key].name}"/>`
+    cardTextMember += `
+    <h3>${membersTeam[key].name}</h3> 
+    <p>${membersTeam[key].role}</p>`
+
+    let provauno = contCardImgTeam.append = cardMember;
+    let provadue = contCardTextTeam.append = cardTextMember;
+    let provatre = contMembersTeam.append = provauno + provadue;
+    contTeams.append = provatre;
+
+    console.log(provauno);
+    console.log(provadue);
+    console.log(provatre);
+
+    console.log(contCardImgTeam, contCardTextTeam);
+    console.log(key, membersTeam[key]);
+    console.log(membersTeam[key].photo);
+    console.log(membersTeam[key].name);
+    console.log(membersTeam[key].role);
+}
